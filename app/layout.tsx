9d4a695870535f4192e8c66b34d2d48fe30b5177
Rafa <rafael.machado.ampudia01@gmail.com>
2026-08-03 23:13:02 -0300

    setup

---
+++ app/layout.tsx
@@ -0,0 +1,7 @@
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ auth.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+    providers: [GitHub, Google],
+});
+
+interface Provider {
+    id: string;
+    name: string;
+}
