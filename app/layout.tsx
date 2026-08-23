7b7a15ef4fe6e486c4bab3d568e6302074623539
Rafa <rafael.machado.ampudia01@gmail.com>
2026-08-23 18:45:28 -0300

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import "./globals.css";
 import { EdgeStoreProvider } from "@/app/lib/edgestore";
 import { jetbrains_mono } from "./fonts";
+import { Analytics } from "@vercel/analytics/next";
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="pt-BR">
+      <Analytics />
       <body className={`bg-drac-darker ${jetbrains_mono.className}`}>
         <EdgeStoreProvider>{children}</EdgeStoreProvider>
       </body>
